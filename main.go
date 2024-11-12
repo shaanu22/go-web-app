7f6de989135e74099f2ae98955619199d35b85dc
shaanu22 <mails2samtaiwo@gmail.com>
2024-11-12 14:59:12 -0600

    Updated main.go

--- main.go
+++ main.go
@@ -26,7 +26,6 @@ func contactPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
 	http.HandleFunc("/home", homePage)
 	http.HandleFunc("/courses", coursePage)
 	http.HandleFunc("/about", aboutPage)
